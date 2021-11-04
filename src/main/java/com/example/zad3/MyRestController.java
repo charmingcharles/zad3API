f950116d8c15ecf089d60374a60c35f599db0048
klesniewski50 <g3zv1ag8dx1ms2@gmail.com>
2021-11-04 13:53:28 +0100

    dodanie parsowania na xml

--- src/main/java/com/example/zad3/MyRestController.java
+++ src/main/java/com/example/zad3/MyRestController.java
@@ -1,9 +1,14 @@
 package com.example.zad3;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.enterprise.inject.Produces;
 
 @RestController
 public class MyRestController {
@@ -13,15 +18,18 @@ public class MyRestController {
         return text;
     } //can be used for testing connection
 
-    @RequestMapping(value = "/apiresult") //example: http://localhost:8090/apiresult?text=Karol
+    @RequestMapping(value = "/apiresult", produces=MediaType.APPLICATION_XML_VALUE) //example: http://localhost:8090/apiresult?text=Karol
     public String apiresult(@RequestParam(value = "text", defaultValue = "Karol") String text) {
         final String uri = "http://localhost:8080/jsonitself?text=" + text;
 
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
 
-        System.out.println(result);
-        return result;
-    }
+        XStream xStream = new XStream(new DomDriver());
+        xStream.alias("map", java.util.Map.class);
+        String xml = xStream.toXML(result);
+        System.out.println(xml);
+        return xml;
+    } //temporary function for local testing
 
 }
