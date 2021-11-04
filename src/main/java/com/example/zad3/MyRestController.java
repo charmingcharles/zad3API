16d2c9e8500032de0f4c6c51c2c10cd27e394d8f
klesniewski50 <g3zv1ag8dx1ms2@gmail.com>
2021-11-04 13:13:48 +0100

    podbieranie danych z API z zadania 2

--- src/main/java/com/example/zad3/MyRestController.java
+++ src/main/java/com/example/zad3/MyRestController.java
@@ -3,6 +3,7 @@ package com.example.zad3;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MyRestController {
@@ -11,5 +12,16 @@ public class MyRestController {
     public String itself(@RequestParam(value = "text", defaultValue = "Karol") String text) {
         return text;
     } //can be used for testing connection
+
+    @RequestMapping(value = "/apiresult") //example: http://localhost:8090/apiresult?text=Karol
+    public String apiresult(@RequestParam(value = "text", defaultValue = "Karol") String text) {
+        final String uri = "http://localhost:8080/jsonitself?text=" + text;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        System.out.println(result);
+        return result;
+    }
 
 }
